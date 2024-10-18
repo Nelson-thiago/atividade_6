3ec98bcf58ebe9dd5f1db2faf979b830050bcfe9
Lucas Andrade <academicolucas>
2024-10-18 10:48:55 -0400

    update bot.py and app.py with function obter_veiculo

--- bot/bot.py
+++ bot/bot.py
@@ -1,11 +1,10 @@
 from botcity.web import WebBot, Browser, By
 from botcity.maestro import *
 from webdriver_manager.chrome import ChromeDriverManager
+from classes import Veiculo, Carro, Motocicleta
+from app import obter_veiculos
+
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
-
-# Importação da classe formulario
-# from formulario import FormLogin
-
 
 def main():
     maestro = BotMaestroSDK.from_sys_args()
@@ -18,73 +17,48 @@ def main():
     bot.browser = Browser.CHROME
     bot.driver_path = ChromeDriverManager().install()
 
-    from classes import Veiculo
-    from classes import Carro
-    from classes import Motocicleta
-
     print("Iniciando a automação do formulário de produto.")
 
-    # Instânciando objetos
-    carro = Carro("Ford", "Fiesta", 2020, 100, "Gasolina")
-    moto = Motocicleta("Honda", "CB500", 2021, 80, 250)
+    # Obtendo a lista de veículos do arquivo
+    veiculos = obter_veiculos()
 
     # Local Menu - CONFIGURAR CONFORME MÁQUINA
     bot.browse(r'C:\Users\matutino\Desktop\desafio\atividade_6\templates\menu.html')
 
-    # ---Processo de alugar carro
-    bot.find_element('/html/body/ul/li[1]/a/button', By.XPATH).click()
-    bot.wait(1000)
-    bot.find_element('//*[@id="tipo_veiculo"]', By.XPATH).send_keys(carro.tipo_veiculo)
-    bot.wait(1000)
-    bot.find_element('//*[@id="marca"]', By.XPATH).send_keys(carro.tipo_veiculo)
-    bot.wait(1000)
-    bot.find_element('//*[@id="modelo"]', By.XPATH).send_keys(carro.modelo)
-    bot.wait(1000)
-    bot.find_element('//*[@id="ano"]', By.XPATH).send_keys(carro.ano)
-    bot.wait(1000)
-    bot.find_element('//*[@id="diaria"]', By.XPATH).send_keys(carro.valor_diario)
-    bot.wait(1000)
-    bot.find_element('//*[@id="combustivel"]', By.XPATH).send_keys(carro.tipo_combustivel)
-    bot.wait(1000)
-    bot.find_element('/html/body/form/input[7]', By.XPATH).click()
-    bot.wait(1000)
+    # ---Processo de alugar veículos
+    for veiculo in veiculos:
+        bot.find_element('/html/body/ul/li[1]/a/button', By.XPATH).click()
+        bot.wait(1000)
+        bot.find_element('//*[@id="tipo_veiculo"]', By.XPATH).send_keys(veiculo.tipo_veiculo)
+        bot.wait(1000)
+        bot.find_element('//*[@id="marca"]', By.XPATH).send_keys(veiculo.marca)
+        bot.wait(1000)
+        bot.find_element('//*[@id="modelo"]', By.XPATH).send_keys(veiculo.modelo)
+        bot.wait(1000)
+        bot.find_element('//*[@id="ano"]', By.XPATH).send_keys(veiculo.ano)
+        bot.wait(1000)
+        bot.find_element('//*[@id="diaria"]', By.XPATH).send_keys(veiculo.valor_diario)
 
-    # Botão de voltar para o menu principal !!!!!
-    bot.find_element('', By.XPATH).click()
-    bot.wait(1000)
+        # Verifique o tipo de veículo para preencher os campos apropriados
+        if isinstance(veiculo, Carro):
+            bot.find_element('//*[@id="combustivel"]', By.XPATH).send_keys(veiculo.tipo_combustivel)
+        elif isinstance(veiculo, Motocicleta):
+            bot.find_element('//*[@id="cc"]', By.XPATH).send_keys(veiculo.cilindrada)
 
-    # Botão de ir para o processo de alugar moto
-    bot.find_element('/html/body/ul/li[2]/a/button', By.XPATH).click()
-    bot.wait(1000)
+        bot.wait(1000)
+        bot.find_element('/html/body/form/input[7]', By.XPATH).click()
+        bot.wait(1000)
 
-    # ---- Processo de alugar moto
-    bot.find_element('//*[@id="tipo_veiculo"]', By.XPATH).send_keys(moto.tipo_veiculo)
-    bot.wait(1000)
-    bot.find_element('//*[@id="marca"]', By.XPATH).send_keys(moto.marca)
-    bot.wait(1000)
-    bot.find_element('//*[@id="modelo"]', By.XPATH).send_keys(moto.modelo)
-    bot.wait(1000)
-    bot.find_element('//*[@id="ano"]', By.XPATH).send_keys(moto.ano)
-    bot.wait(1000)
-    bot.find_element('//*[@id="diaria"]', By.XPATH).send_keys(moto.valor_diario)
-    bot.wait(1000)
-    bot.find_element('//*[@id="cc"]', By.XPATH).send_keys(moto.cilindrada)
-    bot.wait(1000)
-    bot.find_element('/html/body/form/input[7]', By.XPATH).click()
-    bot.wait(1000)
+        # Botão de voltar para o menu principal
+        bot.find_element('', By.XPATH).click()
+        bot.wait(1000)
 
-    # Botão de voltar para o menu principal !!!!!
-    bot.find_element('', By.XPATH).click()
-    bot.wait(1000)
-
-    # Botão de ir para listar veiculos
+    # Botão de ir para listar veículos
     bot.find_element('/html/body/ul/li[3]/a/button', By.XPATH).click()
     bot.wait(1000)
 
-
 def not_found(label):
     print(f"Element not found: {label}")
-
 
 if __name__ == '__main__':
     main()
