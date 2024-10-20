b59d69bafca6ae5ed29930d7e88f32c6cd916edf
Lucas Andrade <academicolucas93@gmail.com>
2024-10-20 19:11:01 -0400

    a

--- bot/bot.py
+++ bot/bot.py
@@ -7,7 +7,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from classes.carro import Carro
-from classes.motocicleta import Motocicleta
+from classes.motociclet import Motocicleta
 from app import obter_veiculos
 
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
